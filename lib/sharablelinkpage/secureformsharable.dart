be09dcb09c0013d51ebe7c81a316c1214c03b31c
Edwin Awariyah <edwin.awariyah@outlook.com>
2024-08-24 19:06:04 +0000

    lgtm

--- lib/sharablelinkpage/secureformsharable.dart
+++ lib/sharablelinkpage/secureformsharable.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-// import 'package:share_plus/share_plus.dart';
-
-// import 'package:share_plus/share_plus.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
+import 'dart:typed_data';
+import 'package:dio/dio.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class SecureFormSharable extends StatefulWidget {
   const SecureFormSharable({super.key});
@@ -22,6 +18,29 @@ class _SecureFormSharableState extends State<SecureFormSharable> {
   }
 
   TextEditingController controller = TextEditingController();
+
+  final String imageUrl = 'https://eawariyah.github.io/qlear/QRCODE.png';
+
+  Future<void> saveQRCode() async {
+    // Request permission to access photos/media/gallery
+    var permissionStatus = await Permission.storage.request();
+    if (permissionStatus.isGranted) {
+      // Download image
+      var response = await Dio()
+          .get(imageUrl, options: Options(responseType: ResponseType.bytes));
+
+      // Save image to gallery
+      final result = await ImageGallerySaver.saveImage(
+        Uint8List.fromList(response.data),
+        quality: 100,
+        name: "qrcode_image",
+      );
+      print(
+          result); // You can use this to check if the image was saved successfully
+    } else {
+      print('Permission not granted. Unable to save QR code.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,107 +67,93 @@ class _SecureFormSharableState extends State<SecureFormSharable> {
               children: [
                 Container(
                   width: MediaQuery.sizeOf(context).width,
-                  height: MediaQuery.sizeOf(context).height / 2,
+                  height: MediaQuery.sizeOf(context).height * 0.45,
                   color: Colors.black,
-                  child: Container(
-                    width: 200,
-                    height: 200,
-                    child: Image.network('https://codet.io/CodetMobile.png'),
-                  ),
-                ),
-                Container(
-                  // width: MediaQuery.sizeOf(context).width,
-                  // height: MediaQuery.sizeOf(context).height / 2,
                   child: Column(
                     children: [
-                      // Container(
-                      //   color: Colors.black,
-                      //   height: 200,
-                      //   width: MediaQuery.sizeOf(context).width,
-                      //   child: Image.network(
-                      //       'https://codet.io/CodetMobile.png' +
-                      //           controller.text,
-                      //       fit: BoxFit.cover),
-                      //   // Image.asset(
-                      //   //   "assets/QRCODE.png",
-                      //   //   width: 200,
-                      //   //   height: 200,
-                      //   // ),
-                      // ),
-                      Container(
-                          color: Colors.black,
-                          width: MediaQuery.sizeOf(context).width,
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 20, bottom: 20),
-                            child: Center(
-                              child: ElevatedButton(
-                                onPressed: null,
-                                style: ButtonStyle(
-                                    backgroundColor: WidgetStateProperty.all(
-                                        Color(0xFF1E1E1E))),
-                                child: Text('Save QR code',
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontFamily: "Poppins",
-                                        color: Colors.white)),
-                              ),
-                            ),
-                          )),
-                    ],
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Clipboard.setData(new ClipboardData(text: controller.text))
-                        .then((_) {
-                      controller.clear();
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text('Copied to your clipboard !')));
-                    });
-                  },
-                  child: Column(
-                    children: [
-                      Text(
-                        'www.qrcodeplus.com/iuadsnif45' + controller.text,
-                        style: TextStyle(
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                        child: Image.network(
+                          imageUrl,
+                          height: MediaQuery.sizeOf(context).height * 0.3,
+                        ),
+                      ),
+                      Spacer(),
+                      ElevatedButton(
+                        onPressed: saveQRCode,
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Color(0xFF1E1E1E)),
+                        ),
+                        child: Text(
+                          'Save Sharable QR Code',
+                          style: TextStyle(
                             fontSize: 20,
                             fontFamily: "Poppins",
-                            color: Colors.black),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Container(
-                        width: 150,
-                        height: 80,
-                        child: ElevatedButton(
-                          onPressed: null,
-                          style: ButtonStyle(
-                            padding: WidgetStateProperty.all<EdgeInsets>(
-                                EdgeInsets.all(10)), // Set padding to zero
-                            shape:
-                                WidgetStateProperty.all<RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10))),
-                            backgroundColor:
-                                WidgetStateProperty.all(Color(0xFF1E1E1E)),
-                          ),
-                          child: Column(
-                            children: [
-                              Icon(Icons.share, color: Colors.white),
-                              Text('Share link',
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontFamily: "Poppins",
-                                      color: Colors.white)),
-                            ],
+                            color: Colors.white,
                           ),
                         ),
                       ),
+                      Spacer()
                     ],
                   ),
                 ),
+                Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  color: Colors.amber,
+                ),
+                // ElevatedButton(
+                //   onPressed: () {
+                //     Clipboard.setData(new ClipboardData(text: controller.text))
+                //         .then((_) {
+                //       controller.clear();
+                //       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                //           content: Text('Copied to your clipboard !')));
+                //     });
+                //   },
+                //   child: Column(
+                //     children: [
+                //       Text(
+                //         'www.qrcodeplus.com/iuadsnif45' + controller.text,
+                //         style: TextStyle(
+                //             fontSize: 20,
+                //             fontFamily: "Poppins",
+                //             color: Colors.black),
+                //       ),
+                //       SizedBox(
+                //         height: 30,
+                //       ),
+                //       Container(
+                //         width: 150,
+                //         height: 80,
+                //         child: ElevatedButton(
+                //           onPressed: null,
+                //           style: ButtonStyle(
+                //             padding: WidgetStateProperty.all<EdgeInsets>(
+                //                 EdgeInsets.all(10)), // Set padding to zero
+                //             shape:
+                //                 WidgetStateProperty.all<RoundedRectangleBorder>(
+                //                     RoundedRectangleBorder(
+                //                         borderRadius:
+                //                             BorderRadius.circular(10))),
+                //             backgroundColor:
+                //                 WidgetStateProperty.all(Color(0xFF1E1E1E)),
+                //           ),
+                //           child: Column(
+                //             children: [
+                //               Icon(Icons.share, color: Colors.white),
+                //               Text('Share link',
+                //                   style: TextStyle(
+                //                       fontSize: 20,
+                //                       fontFamily: "Poppins",
+                //                       color: Colors.white)),
+                //             ],
+                //           ),
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                // ),
                 const Spacer(),
                 BottomNavigationBar(
                   items: const [
